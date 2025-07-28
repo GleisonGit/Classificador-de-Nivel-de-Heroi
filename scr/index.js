5accb5b3a039048ba5a3f82d63970c2c1d312bc6
Mendes <165393441+GleisonGit@users.noreply.github.com>
2025-07-28 02:51:13 -0300

    Herói

---
+++ scr/index.js
@@ -0,0 +1,29 @@
+// Variáveis para armazenar o nome e a XP do herói
+let nomeHeroi = "He-Man";
+let xpHeroi = 9950; // Você pode mudar a XP aqui para testar diferentes níveis!
+
+// Variável para armazenar o nível do herói
+let nivelHeroi;
+
+// Estrutura de decisão para determinar o nível do herói com base na XP
+if (xpHeroi < 1000) {
+  nivelHeroi = "Ferro";
+} else if (xpHeroi >= 1001 && xpHeroi <= 2000) {
+  nivelHeroi = "Bronze";
+} else if (xpHeroi >= 2001 && xpHeroi <= 5000) {
+  nivelHeroi = "Prata";
+} else if (xpHeroi >= 5001 && xpHeroi <= 7000) {
+  nivelHeroi = "Ouro";
+} else if (xpHeroi >= 7001 && xpHeroi <= 8000) {
+  nivelHeroi = "Platina";
+} else if (xpHeroi >= 8001 && xpHeroi <= 9000) {
+  nivelHeroi = "Ascendente";
+} else if (xpHeroi >= 9001 && xpHeroi <= 10000) {
+  nivelHeroi = "Imortal";
+} else {
+  // xpHeroi >= 10001
+  nivelHeroi = "Radiante";
+}
+
+// Exibe a mensagem final sem os asteriscos de Markdown
+console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivelHeroi}`);
